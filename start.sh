c611137fcef4ecea2c4f66e286783c3925967251
Leon Gr <Leon4gr45@users.noreply.huggingface.co>
2026-07-07 10:12:39 +0000

    Upload folder using huggingface_hub

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python -c "import os, gradio; path=os.path.join(os.path.dirname(gradio.__file__), 'components', 'base.py'); c=open(path).read(); open(path, 'w').write(c.replace('getattr(self, value)', 'getattr(self, value) if value != \"__provides__\" else None'))"
 python app.py
+# Trigger upload
